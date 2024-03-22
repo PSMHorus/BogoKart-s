7c4ce2eed444287d5363b232ca781f14bdd33ed4
PSMHorus <andrs1996@gmail.com>
2024-03-22 01:06:07 -0500

    CarMovement mejorado
    este codigo tiene un mejor balance para el derrape pero debemos ponerlo con el modelo realizado para dar el visto bueno

--- Assets/Scripts/CarMovementVariable.cs
+++ Assets/Scripts/CarMovementVariable.cs
@@ -1,65 +1,99 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CarMovementVariable : MonoBehaviour
 {
-    public float moveSpeed = 50;
-    public float Drag = 0.98f;
-    public float MaxSpeed = 15;
-    public float Traction = 1;
-    public float SteerAngle = 20;
-    private Vector3 MoveForce;
-
-    private bool IsDrifting =false;
-
-    void Update()
+    public enum Axel
     {
-        MoveForce += transform.forward * moveSpeed * Input.GetAxis("Vertical") * Time.deltaTime;
-        transform.position += MoveForce * Time.deltaTime;
-
-
-        float steerInput = Input.GetAxis("Horizontal");
-        if (Input.GetKey(KeyCode.Space)) 
-        {
-            IsDrifting=true;
-        }
-
-        else 
-        {
-            IsDrifting=false;
-        }
-
-        if (IsDrifting) 
-        {
-            
-            transform.Rotate(Vector3.up * steerInput * SteerAngle * Time.deltaTime);
-        }
-
-        else
-        {
-            MoveForce *= Drag;
-            transform.Rotate(Vector3.up * steerInput * MoveForce.magnitude * SteerAngle * Time.deltaTime);
-        }
-        // transform.Rotate(Vector3.up*steerInput*MoveForce.magnitude*SteerAngle*Time.deltaTime);
-
-
-
-        
-        MoveForce = Vector3.ClampMagnitude(MoveForce, MaxSpeed);
-
-
-
-        Debug.DrawRay(transform.position, MoveForce.normalized * 3);
-        Debug.DrawRay(transform.position,transform.forward * 3,Color.blue);
-        MoveForce = Vector3.Lerp(MoveForce.normalized,transform.forward,Traction*Time.deltaTime) *MoveForce.magnitude;
-
+        Front,
+        Rear,
     }
-    
 
+    [Serializable]
+    public struct Wheel
+    {
+        public GameObject wheelModel;
+        public WheelCollider wheelCollider;
+        public Axel axel;
+    }
 
+    public float maxAcceleration = 30.0f;
+    public float breakAcceleration = 50.0f;
+    public float turnSensitive = 1.0f;
+    public float maxSteerAngle = 30.0f;
+    public Vector3 centerOfMass;
+    public List<Wheel> wheels;
 
+    private float moveInput;
+    private float steerInput;
+    private Rigidbody carRig;
 
+    void Start()
+    {
+        carRig = GetComponent<Rigidbody>();
+        carRig.centerOfMass = centerOfMass;
+    }
 
+    private void Update()
+    {
+        GetInput();
+        AnimationWheels();
+    }
 
+    void LateUpdate()
+    {
+        Move();
+        Steer();
+        Brake();
+    }
+
+    void GetInput()
+    {
+        moveInput = Input.GetAxis("Vertical");
+        steerInput = Input.GetAxis("Horizontal");
+    }
+
+    void Move()
+    {
+        foreach (var wheel in wheels)
+        {
+            wheel.wheelCollider.motorTorque = moveInput * 600 * maxAcceleration * Time.deltaTime;
+        }
+    }
+
+    void Steer()
+    {
+        foreach (var wheel in wheels)
+        {
+            if (wheel.axel == Axel.Front)
+            {
+                float steerAngle = steerInput * turnSensitive * maxSteerAngle;
+                wheel.wheelCollider.steerAngle = Mathf.Lerp(wheel.wheelCollider.steerAngle, steerAngle, 0.6f);
+            }
+        }
+    }
+
+    void Brake()
+    {
+        float brakeTorque = Input.GetKey(KeyCode.Space) ? 800 * breakAcceleration * Time.deltaTime : 0;
+
+        foreach (var wheel in wheels)
+        {
+            wheel.wheelCollider.brakeTorque = brakeTorque;
+        }
+    }
+
+    void AnimationWheels()
+    {
+        foreach (var wheel in wheels)
+        {
+            Quaternion rot;
+            Vector3 pos;
+            wheel.wheelCollider.GetWorldPose(out pos, out rot);
+            wheel.wheelModel.transform.position = pos;
+            wheel.wheelModel.transform.rotation = rot;
+        }
+    }
 }
